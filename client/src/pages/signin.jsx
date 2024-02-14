f7b3b709e8b6e752db361cb97bbf967d805c3988
Arslan <arslanabbas5511@gmail.com>
2024-02-14 22:06:22 +0500

    create api for email login from google and interface of signin page

---
+++ client/src/pages/signin.jsx
@@ -0,0 +1,43 @@
+
+import { CHECK_USER_ROUTE } from '@/utils/ApiRoutes'
+import { auth } from '@/utils/FirebaseConfig'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import React from 'react'
+import {FcGoogle} from 'react-icons/fc'
+
+function signin() {
+  const router = useRouter()
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const {
+      user : { displayName:name, email, photoUrl: profileImage },
+      } = await signInWithPopup(auth, provider);
+    try {
+      if(email){
+        const {data} = await axios.post(CHECK_USER_ROUTE,{ email })
+        console.log({data})
+        if(!data.status){
+          router.push("/onboarding")
+        }
+      }
+    }catch(err){
+      console.log(err)}
+    }
+  return (
+    <div className='flex justify-center items-center bg-panel-header-background flex-col w-screen h-screen gap-6'>
+      <div className='flex items-center justify-center'>
+        <Image src='/whatsapp.gif' width={300} height={300} alt='whatsapp' />
+        <span className='text-7xl text-white'>WhatsApp</span>
+      </div>
+      <button className='flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg ' onClick={handleLogin} >
+        <FcGoogle className='text-4xl' />
+      <span className='text-white text-2xl'>Login With Google</span>
+      </button>
+    </div>
+  )
+}
+
+export default signin;
+
---
+++ client/src/utils/ApiRoutes.js
@@ -0,0 +1,5 @@
+export const HOST = "http://localhost:3007";
+
+const AuthRoutes = `${HOST}/api/auty`;
+
+export const CHECK_USER_ROUTE = `${AuthRoutes}/check-user`
