088c5cae5f69a0f502ef4ae6aa4c2ec213aa77cf
Arslan <arslanabbas5511@gmail.com>
2024-02-15 21:23:54 +0500

    Axios added, Resolved Errors

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -2,6 +2,6 @@
 
 export const HOST = "http://localhost:3007";
 
-const AuthRoutes = `${HOST}/api/auth`;
+const AUTH_ROUTE = `${HOST}/api/auth`;
 
-export const CHECK_USER_ROUTE = `${AuthRoutes}/check-user`
+export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -2,18 +2,14 @@ import getPrismaInstance from "../utils/PrismaClient.js";
 
 
 
-export const checkUser = async (res,req,next) => {
+export const checkUser = async (req,res,next) => {
     try {
         const {email} = req.body;
         if(!email){
             return res.json({ msg:'Email is required', status: false })
         }
         const prisma = getPrismaInstance();
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+        const user = await prisma.user.findUnique({where: {email}})
 
         if(!user){
             return res.json({ msg:'User not found', status: false })
