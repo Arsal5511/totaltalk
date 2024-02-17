94c07cc9bb114b7a8f3e06cddde6043f95f7b390
Arslan <arslanabbas5511@gmail.com>
2024-02-17 12:30:03 +0500

    ContextMenu Component Completed and working fine

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,81 +1,84 @@
 import Image from "next/image";
 import React, { useState } from "react";
-import {FaCamera} from 'react-icons/fa'
+import { FaCamera } from "react-icons/fa";
+import ContextMenu from "./ContextMenu";
 
-function Avatar({type, image, setImage}) {
-
-  const [hover, setHover] = useState(false)
-  const [isContextMenuVisible, setisContextMenuVisible] = useState(false)
+function Avatar({ type, image, setImage }) {
+  const [hover, setHover] = useState(false);
+  const [isContextMenuVisible, setisContextMenuVisible] = useState(false);
   const [constextMenuCordinates, setisContextMenuCordinates] = useState({
     x: 0,
-    y: 0
-  })
+    y: 0,
+  });
 
   const showContextMenu = (e) => {
-    e.preventDefault()
-    setisContextMenuVisible(true)
+    e.preventDefault();
+    setisContextMenuVisible(true);
     setisContextMenuCordinates({
       x: e.clientX,
-      y: e.clientY
-    })
-  }
+      y: e.clientY,
+    });
+  };
 
-  // const hideContextMenu = () => {
-  //   setisContextMenuVisible(false)
-  //   setisContextMenuCordinates({
-  //     x: 0,
-  //     y: 0
-  //   })
-  // }
+  const contextMenuOptions = [{ name: "Take Photo", callback: () => {} },
+  { name: "Chose From Library", callback: () => {} },
+  { name: "Upload Photo", callback: () => {} },
+  { name: "Remove Photo", callback: () => {} }];
 
- 
   return (
     <>
-    <div className="flex items-center justify-center ">
-      {
-        type==="sm" && (
+      <div className="flex items-center justify-center ">
+        {type === "sm" && (
           <div className="relative h-10 w-10">
-          <Image src={image} alt="avatar" className="rounded-full" fill />
+            <Image src={image} alt="avatar" className="rounded-full" fill />
           </div>
-        )
-      }
-      {
-        type==="lg" && (
+        )}
+        {type === "lg" && (
           <div className="relative h-14 w-14">
-          <Image src={image} alt="avatar" className="rounded-full" fill  />
+            <Image src={image} alt="avatar" className="rounded-full" fill />
           </div>
-        )
-      }
-      {
-        type==="xl" && (
+        )}
+        {type === "xl" && (
           <div
-           className="relative cursor-pointer z-0"
-           onMouseEnter={() => setHover(true)}
-           onMouseLeave={() => setHover(false)}
+            className="relative cursor-pointer z-0"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
           >
-          <div className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center flex-col justify-center rounded-full gap-2 ${hover ? "visible" : "hidden"}`} onClick={e =>showContextMenu(e)}
-          id="context-opener"
-          >
-            <FaCamera
-            onClick={e =>showContextMenu(e)}
-            className="text-2xl"
-            id="context-opener" />
-            <span
-            id="context-opener"
-            onClick={e =>showContextMenu(e)}
-            >Change<br/>Profile<br/>Photo</span>
-             
+            <div
+              className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center flex-col justify-center rounded-full gap-2 ${
+                hover ? "visible" : "hidden"
+              }`}
+              onClick={(e) => showContextMenu(e)}
+              id="context-opener"
+            >
+              <FaCamera
+                onClick={(e) => showContextMenu(e)}
+                className="text-2xl"
+                id="context-opener"
+              />
+              <span id="context-opener" onClick={(e) => showContextMenu(e)}>
+                Change
+                <br />
+                Profile
+                <br />
+                Photo
+              </span>
+            </div>
+            <div className="flex items-center justify-center h-60 w-60">
+              <Image src={image} alt="avatar" className="rounded-full" fill />
+            </div>
           </div>
-          <div className="flex items-center justify-center h-60 w-60">
-          <Image src={image} alt="avatar" className="rounded-full" fill  />
-          </div>
-          </div>
-        )
-      }
-      
-    </div>
+        )}
+        {isContextMenuVisible && (
+          <ContextMenu
+            option={contextMenuOptions}
+            cordinates={constextMenuCordinates}
+            setContextMenu={setisContextMenuVisible}
+            ContextMenu={isContextMenuVisible}
+          />
+        )}
+      </div>
     </>
-
   );
 }
 
--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-function ContextMenu({option, cordinates, ContextMenu, setisContextMenu}) {
+function ContextMenu({ option, cordinates, ContextMenu, setContextMenu }) {
+  const contextMenuRef = useRef(null);
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if(event.target.id !== "context-opener"){
+        if(contextMenuRef.current && !contextMenuRef.current.contains(event.target)){
+          setContextMenu(false);
+        }
+      }
+    }
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+  
+  }, [])
+  
+  const handleClick = (e, callback) => {
+    e.stopPropagation();
+    setContextMenu(false);
+    callback();
+  };
   return (
-  <div>ContextMenu</div>);
+    <div
+      className="bg-dropdown-background fixed py-2 z-[100] shadow-xl "
+      ref={contextMenuRef}
+      style={{ top: cordinates.y, left: cordinates.x }}
+    >
+      <ul>
+        {option.map(({ name, callback }) => (
+          <li
+            className="px-5 py-2 cursor-pointer hover:bg-background-default-hover "
+            key={name}
+            onClick={(e)=>handleClick(e, callback)}
+          >
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
