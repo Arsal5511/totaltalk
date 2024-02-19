0dd3eebc0af2cac7e56fff828793c74d04d976ff
Arslan <arslanabbas5511@gmail.com>
2024-02-19 22:50:28 +0500

    Photo Library Commenting

--- client/src/components/common/PhotoLibrary.jsx
+++ client/src/components/common/PhotoLibrary.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { IoClose } from "react-icons/io5";
 
+// array of photos for library
 function PhotoLibrary({setImage, hidePhotoLibrary}) {
   const images = [
     "/avatars/1.png",
@@ -15,11 +16,18 @@ function PhotoLibrary({setImage, hidePhotoLibrary}) {
     "/avatars/9.png",
 
   ];
-  return <div className="fixed flex top-0 left-0 max-h-[100vh] max-w-[100vw] h-full w-full justify-center items-center ">
+
+  return (
+  <div className="fixed flex top-0 left-0 max-h-[100vh] max-w-[100vw] h-full w-full justify-center items-center ">
+
     <div className="h-max w-max bg-gray-900 gap-6 rounded-lg p-4 " >
+
+      {/* Close Button */}
       <div className=" pt-2  pr-2 cursor-pointer flex items-end justify-end "  onClick={() => hidePhotoLibrary(false)}>
       <IoClose className=" h-10 w-10 " />
       </div>
+
+      {/* map images */}
       <div className="grid grid-cols-3 justify-center items-center gap-16 p-20 w-full ">
         {images.map((image, index) => (
           <div
@@ -35,8 +43,11 @@ function PhotoLibrary({setImage, hidePhotoLibrary}) {
           
         ))}
       </div>
+
     </div>
-  </div>;
+    
+  </div>
+);
 }
 
 export default PhotoLibrary;
