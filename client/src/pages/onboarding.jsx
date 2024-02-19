5885479999adf39d55982e9607d27b4400330152
Arslan <arslanabbas5511@gmail.com>
2024-02-19 22:40:05 +0500

    Onboarding page commenting

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -8,24 +8,33 @@ function onboarding() {
   const [{userInfo}] = useStateProvider()
   const [name, setName] = useState(userInfo?.name ||"")
   const [about, setAbout] = useState(userInfo?.name ||"")
+  //default avatar picture
   const [image, setImage] = useState("/default_avatar.png")
-  console.log(image)
   return (
+
   <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center ">
+
     <div className="flex items-center justify-center gap-2 ">
       <Image src='/whatsapp.gif' alt="whatsapp" height={300} width={300} priority />
       <span className="text-7xl" >Whatsapp</span>
     </div>
+
     <h2 className="text-2xl">Create your Profile</h2>
+
+
     <div className="flex gap-6 mt-6">
+
+    {/* Input.jsx  component for user name and about */}
       <div className="flex flex-col items-center justify-center mt-5 gap-6  ">
         <Input name="Display Name" state={name} setState={setName} label />
         <Input name="About" state={about} setState={setAbout} label />
-
       </div>
+
+    {/* Avatar.jsx component for user picture */}
       <div>
         <Avatar type="xl" image={image} setImage={setImage} />
       </div>
+      
     </div>
 
   </div>
