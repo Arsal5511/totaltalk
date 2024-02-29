fd4398271d230453a580e368f90530f759a6971a
Arslan <arslanabbas5511@gmail.com>
2024-02-29 23:20:36 +0500

    some styles corrected

--- client/src/components/common/CapturePhoto.jsx
+++ client/src/components/common/CapturePhoto.jsx
@@ -15,8 +15,12 @@ function CapturePhoto({hide, setImage}) {
     videoRef.current.srcObject = stream;
    }
    startCamera();
+
    return () => {
-    stream?.getTracks.forEach((track)=>track.stop())
+
+    if (stream) {
+        stream.getTracks().forEach((track) => track.stop());
+      }
 
    }
   },)
@@ -33,13 +37,13 @@ function CapturePhoto({hide, setImage}) {
 
   return (
     <>
-        <div className="absolute h-4/6 w-2/6 top-1/3 left-1/3 bg-gray-900 gap-3 rounded-lg pt-2 flex items-center justify-center " >
-          <div className="flex flex-col gap-4 w-full items-center justify-center ">
+        <div className="absolute h-4/6 w-2/6 top-1/4 left-1/3 bg-gray-900 gap-3 rounded-lg pt-2 flex items-center justify-center " >
+          <div className="flex flex-col gap-4 w-full h-full items-center justify-center ">
             <div className=" pt-2  pr-2 cursor-pointer flex items-end justify-end "  onClick={() => hide(false)}>
               <IoClose className=" h-10 w-10 " />
             </div>
             <div className="flex justify-center ">
-              <video src="" id="video" width={400} autoPlay ref={videoRef}></video>
+              <video src="" id="video" width={300} height={300} autoPlay ref={videoRef}></video>
             </div>
             <button
             className="h-16 w-16 bg-white rounded-full cursor-pointer border-8 border-teal-light p-2 mb-10 "
