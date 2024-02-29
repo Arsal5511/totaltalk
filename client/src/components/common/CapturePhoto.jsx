c015474388253c2c179deb17c3a3e6c261bd229b
Arslan <arslanabbas5511@gmail.com>
2024-02-29 21:53:21 +0500

    minor change

--- client/src/components/common/CapturePhoto.jsx
+++ client/src/components/common/CapturePhoto.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { IoClose } from "react-icons/io5";
 
 
-function CapturePhoto() {
+function CapturePhoto({hide, setImage}) {
   return (
     <>
         <div className="absolute h-4/6 w-2/6 top-1/4 left-1/3 bg-gray-900 gap-3 rounded-lg pt-2 flex items-center justify-center " >
           <div className="flex flex-col gap-4 w-full">
-            <div className=" pt-2  pr-2 cursor-pointer flex items-end justify-end "  onClick={() => hidePhotoLibrary(false)}>
+            <div className=" pt-2  pr-2 cursor-pointer flex items-end justify-end "  onClick={() => hide(false)}>
               <IoClose className=" h-10 w-10 " />
             </div>
+            <div className="flex"></div>
           </div>
         </div>
    </>
