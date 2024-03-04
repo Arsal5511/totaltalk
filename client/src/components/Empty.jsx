c4569f3dad455fd3fef9a6a3d072fa3e1715a5ea
Arslan <arslanabbas5511@gmail.com>
2024-03-04 20:33:45 +0500

    interface when no chat is selected in Empty.jsx

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
 import React from "react";
 
 function Empty() {
-  return <div>Empty</div>;
-}
+  return( 
+  <div className="border-conversation-border border-1 w-full h-[100vh] bg-panel-header-background flex flex-col border-b-4 border-b-icon-green items-center justify-center ">
+    <Image src="/whatsapp.gif" alt="whatsapp" width={300} height={300} />
+
+  </div>);}
 
 export default Empty;
