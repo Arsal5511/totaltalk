abbfcf59cde0c046d5e5df06a1faf74fa610bcc6
Arslan <arslanabbas5511@gmail.com>
2024-03-04 22:04:29 +0500

    minor changes

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Empty() {
   return( 
-  <div className="border-conversation-border border-1 w-full h-[100vh] bg-panel-header-background flex flex-col border-b-4 border-b-icon-green items-center justify-center ">
+  <div className="border-conversation-border border-l w-full h-[100vh] bg-panel-header-background flex flex-col border-b-4 border-b-icon-green items-center justify-center ">
     <Image src="/whatsapp.gif" alt="whatsapp" width={300} height={300} />
 
   </div>);}
