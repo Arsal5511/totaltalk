d0ae08b9b2e514c22608228c2082071b584fd64c
Arslan <arslanabbas5511@gmail.com>
2024-03-05 23:56:19 +0500

    working to display contacts in list by fetching it from route

--- client/src/components/Chatlist/ContactsList.jsx
+++ client/src/components/Chatlist/ContactsList.jsx
@@ -2,38 +2,104 @@ import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
 import { GET_ALL_CONTACTS } from "@/utils/ApiRoutes";
 import axios from "axios";
+import { stringify } from "postcss";
 import React, { useEffect, useState } from "react";
-import { BiArrowBack } from "react-icons/bi";
+import { BiArrowBack, BiSearchAlt2 } from "react-icons/bi";
 
 function ContactsList() {
-  const [{}, dispatch ] = useStateProvider();
+  const [{}, dispatch] = useStateProvider();
 
-  const [allContacts, setAllContacts] = useState([])
+  const [allContacts, setAllContacts] = useState([]);
+
+  // useEffect(() => {
+  //   const getContacts = async () => {
+  //     try {
+  //       const {
+  //         data: { users },
+  //       } = await axios.get(GET_ALL_CONTACTS);
+  //       console.log(users)
+  //       setAllContacts(users);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   getContacts();
+  // }, []);
 
   useEffect(() => {
-    const getContacts = async () =>{
-      try{
-
-        const {data:{users}} = await axios.get(GET_ALL_CONTACTS)
-        setAllContacts(users)
-      }catch(err){
-        console.log(err)
-
+    const getContacts = async () => {
+      try {
+        const { data: { users } } = await axios.get(GET_ALL_CONTACTS);
+        console.log(users)
+        setAllContacts(users);
+      } catch (err) {
+        console.log(err);
       }
-    }
-    getContacts()
-  }, [])
-  
+    };
+    getContacts();
+  }, []);
+
   return (
-  <div className="h-full flex flex-col">
-    <div className="h-24 flex items-end px-3 py-4 ">
-      <div className="flex items-center gap-12 text-white">
-        <BiArrowBack className="cursor-pointer text-xl" onClick={() => dispatch({type: reducerCases.SET_ALL_CONTACTS_PAGE, contactsPage: false})} />
+    <div className="h-full flex flex-col">
+      <div className="h-24 flex items-end px-3 py-4 ">
+        <div className="flex items-center gap-12 text-white">
+          <BiArrowBack
+            className="cursor-pointer text-xl"
+            onClick={() =>
+              dispatch({
+                type: reducerCases.SET_ALL_CONTACTS_PAGE,
+                contactsPage: false,
+              })
+            }
+          />
+          <span>New Chat</span>
+        </div>
+      </div>
+
+      <div className="bg-search-input-container-background h-full flex-auto overflow-auto custom-scrollbar">
+
+        <div className="flex py-3 items-center gap-3 h-14">
+
+          <div className="bg-panel-header-background flex items-center gap-5 px-3 py-1 rounded-lg flex-grow mx-4">
+            {/* Search icon  */}
+            <div>
+
+              <BiSearchAlt2 className="text-panel-header-icon cursor-pointer text-lg" />
+
+            </div>
+              {/* input  */}
+            <div>
+
+              <input
+                type="text"
+                placeholder="Search Contacts"
+                className="bg-transparent text-sm focus:outline-none text-white w-full"
+              />
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+        {allContacts && Object.entries(allContacts).map(([initialLetter, userList ]) => {
+            return (
+              <div key={Date.now() + initialLetter}>
+                <div className="text-teal-light pl-10 pr-5">
+                  {initialLetter}
+
+                </div>
+              </div>
+              )
+          })
+        }
+        
+
+
       </div>
     </div>
-
-  </div>
-
-  )}
+  );
+}
 
 export default ContactsList;
