cab68f037cc585bb1c7f8085809e80d9626c4c7e
Arslan <arslanabbas5511@gmail.com>
2024-03-07 22:47:09 +0500

    minor changes

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -2,9 +2,11 @@ import { checkUser, getAllUsers, onBoardUser } from "../controllers/AuthControll
 import {Router} from "express"
 
 const router = Router()
-
+// check user exist or new 
 router.post("/check-user", checkUser)
+// check if user is new or not
 router.post("/onboard-user", onBoardUser)
+// check for all users who are signed in
 router.get("/get-contacts", getAllUsers)
 
 
