96718f34bc9d969509abdfa3775cb12b4829f054
Arslan <arslanabbas5511@gmail.com>
2024-03-07 22:48:32 +0500

    minor changes

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -4,6 +4,11 @@ export const HOST = "http://localhost:3007";
 
 const AUTH_ROUTE = `${HOST}/api/auth`;
 
+
+// API to check user is new or old 
 export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`
+// API for user who is signed in 
 export const ONBOARD_USER_ROUTE = `${AUTH_ROUTE}/onboard-user`
+
+// API for all contacts and users 
 export const GET_ALL_CONTACTS = `${AUTH_ROUTE}/get-contacts`
