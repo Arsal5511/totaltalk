9988984ae45c85e67376852bcc3aa8b776050e22
Arslan <arslanabbas5511@gmail.com>
2024-03-10 23:56:16 +0500

    error is not sorting

--- client/src/components/Chatlist/ContactsList.jsx
+++ client/src/components/Chatlist/ContactsList.jsx
@@ -19,7 +19,7 @@ function ContactsList() {
         const  {data:{users}}  = await axios.get(GET_ALL_CONTACTS);
 
         alert('in try catch')
-        console.log(GET_ALL_CONTACTS)
+        console.log(users)
         setAllContacts(users);
 
       } catch (err) {
