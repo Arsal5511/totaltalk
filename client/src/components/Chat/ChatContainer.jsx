1cc1150dc8f956ea635fa5133c3d05dac0900c04
Arslan <arslanabbas5511@gmail.com>
2024-03-12 21:46:32 +0500

    set schema prisma for messages

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 import React from "react";
 
 function ChatContainer() {
-  return <div className=" h-[80vh] w-full relative flex-grow overflow-auto custom-scrollbar">
-    <div className="bg-chat-background bg-fixed h-full w-full opacity-5 fixed left-0 top-0 z-0">
-      <div className="flex flex-col justify-end w-full gap-1 overflow-auto">
-        
+  return (
+    <div className=" bg-chat-background opacity-5  h-[80vh] w-full relative flex-grow overflow-auto custom-scrollbar">
+      <div className=" bg-fixed   fixed left-0 top-0 z-0">
+        <div className="flex flex-col justify-end w-full gap-1 overflow-auto">
+
+        </div>
       </div>
     </div>
-    
-    </div>;
+  );
 }
 
 export default ChatContainer;
